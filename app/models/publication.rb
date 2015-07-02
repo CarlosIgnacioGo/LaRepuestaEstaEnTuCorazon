99e917670ade586ff4233e06089feaa4f97ed1bd
Carlos Gomez <carlos221519@gmail.com>
2015-07-01 23:12:05 -0400

    Creado el contador de visitas

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,2 +1,13 @@
 class Publication < ActiveRecord::Base
+	before_create :set_visits_count
+
+	def update_visits_count
+		self.update(visits_count: self.visits_count + 1)
+	end
+
+	private
+
+	def set_visits_count
+		self.visits_count = 0
+	end
 end
